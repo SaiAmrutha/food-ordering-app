5d2a4693996228c04ff82fa0e87a9e652a36fadb
Sai Amrutha <nsamrutha@gmail.com>
2025-05-18 23:51:44 +0530

    Initial Commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,16 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   return (
     <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("Button CLicked");
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="search">Search</div>
       <div className="resto-container">
         {/* resto card */}
