ab314106ef1cac7bf62d86180f7c2d0dc9ece305
Sai Amrutha <nsamrutha@gmail.com>
2025-06-10 02:09:11 +0530

    Menu data is being displayed now.

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
